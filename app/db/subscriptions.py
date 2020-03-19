253ed65a1862512dad480bf331d13fda38ea03fd
Michael Burman <yak@iki.fi>
2020-03-19 19:05:37 +0200

    Fix DB query to mail only single account and event_type

--- app/db/subscriptions.py
+++ app/db/subscriptions.py
@@ -24,6 +24,6 @@ async def remove_email_subscription(account_id: str, user_id: str, event_type: s
 
 
 async def get_subscribers(account_id: str, event_type: str):
-    emails: List[EmailSubscription] = await EmailSubscription.query.where((account_id == account_id) &
-                                                                          (event_type == event_type)).gino.all()
+    emails: List[EmailSubscription] = await EmailSubscription.query.where((EmailSubscription.account_id == account_id) &
+                                                                          (EmailSubscription.event_type == event_type)).gino.all()
     return emails
