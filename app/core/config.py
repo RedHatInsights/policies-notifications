1e654ce308bacf7e28cd81889c2c118a445f142b
Michael Burman <yak@iki.fi>
2020-04-02 13:26:14 +0300

    Accept separate DATABASE_HOST, _PORT, _USER and _PASSWORD instead of _URL also

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,15 @@
-from starlette.config import Config
-from starlette.datastructures import URL
+from starlette.config import Config, environ
+from starlette.datastructures import URL, Secret
 
 # Parse configuration file here, make some methods to return certain info..
 
 config = Config('.env')
 
-DATABASE_URL = config('DATABASE_URL', cast=URL)
+DATABASE_HOST = config('DATABASE_HOST', default='')
+DATABASE_PORT = config('DATABASE_PORT', default='')
+DATABASE_USER = config('DATABASE_USER', default='')
+DATABASE_PASSWORD = config('DATABASE_PASSWORD', cast=Secret, default='')
+DATABASE_URL = config('DATABASE_URL', cast=URL, default='postgres://{}:{}@{}:{}/policies_notifications'.format(DATABASE_USER, str(DATABASE_PASSWORD), DATABASE_HOST, DATABASE_PORT))
 KAFKA_BOOTSTRAP_SERVERS = config('KAFKA_BOOTSTRAP_SERVERS')
 KAFKA_QUEUE_HOOK = config('KAFKA_QUEUE_HOOK')
 KAFKA_QUEUE_EMAIL = config('KAFKA_QUEUE_EMAIL')
